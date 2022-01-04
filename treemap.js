b4993455a531121a4fb9d9a39614f57a5fc90422
D20CM <scottbrown500@hotmail.com>
2022-01-04 21:15:05 +0000

    calculate rect areas and apply to canvas

--- treemap.js
+++ treemap.js
@@ -3,21 +3,50 @@ const url =
 
 const width = 600;
 const height = 600;
-const margin = 20;
+const margin = { top: 20, right: 20, bottom: 20, left: 20 };
 
 const svg = d3
   .select("#graph-area")
   .append("svg")
-  .attr("width", width + margin + margin)
-  .attr("height", height + margin + margin)
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
   .append("g")
-  .attr("transform", "translate(" + margin + "," + margin + ")");
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 let treeMap = async function () {
   const response = await fetch(url);
   const dataset = await response.json();
   console.log(dataset);
   const consoles = dataset.children;
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  const root = d3.hierarchy(dataset).sum((d) => d.value);
+
+  d3.treemap().size([width, height]).padding(2)(root);
+
+  svg
+    .selectAll("rect")
+    .data(root.leaves())
+    .enter()
+    .append("rect")
+    .attr("x", function (d) {
+      return d.x0;
+    })
+    .attr("y", function (d) {
+      return d.y0;
+    })
+    .attr("width", function (d) {
+      return d.x1 - d.x0;
+    })
+    .attr("height", function (d) {
+      return d.y1 - d.y0;
+    })
+    .style("stroke", "black")
+    .style("fill", "yellow");
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////
+
   const testArea = document.getElementById("test-area");
 
   function displayConsoles() {
